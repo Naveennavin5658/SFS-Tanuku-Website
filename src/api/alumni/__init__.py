6ec123898f871b6cd518d6f6181a1d9e24e5f9fa
Naveen <ssrikar1@ford.com>
2023-12-27 22:27:42 +0530

    added apis for results and alumni pages

--- src/api/alumni/__init__.py
+++ src/api/alumni/__init__.py
@@ -1,5 +1,5 @@
 '''
-Gallery blueprint handles all apis wrt to images
+Alumni blueprint handles all apis wrt to alumni registration
 '''
 from flask import Blueprint
 alumni_blueprint = Blueprint("alumni",__name__)
