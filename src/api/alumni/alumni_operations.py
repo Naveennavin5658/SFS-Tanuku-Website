de05c07d2afe30014c3cfbfdadae4047a8f03465
Naveen <ssrikar1@ford.com>
2023-12-27 22:27:42 +0530

    added apis for results and alumni pages

---
+++ src/api/alumni/alumni_operations.py
@@ -0,0 +1,93 @@
+from flask import Request
+from scripts import get_db_client
+db_client = get_db_client()
+def add_user_as_alumni(request: Request):
+    try:
+        data = request.get_json(silent=True) or {}
+        email, contact, yop = data.get('email'), data.get('contact'), data.get('passoutYear')
+        if not data:
+            return {"data": "Input request is empty", "code": 400}
+        
+        alumni_collection = db_client.get_collection("alumni")
+        #Check if the user already exists:
+        db_response = list(alumni_collection.find({"email":email,"contact":contact,"passoutYear":yop}))
+        if(db_response!=[]):
+            return {"code":200,"data":"User is already registered!"}
+        else:
+            alumni_collection.insert_one(data)
+            return {"code": 201, "data": "Successfully registered as alumni"}
+    
+    except Exception as e:
+        return {"error": str(e), "code": 500}
+
+
+
+
+
+import boto3
+
+
+def list_buckets():
+    try:
+        s3 = boto3.client('s3')
+        response = s3.list_buckets()
+        if response:
+            print("Buckets exist!")
+            for bucket in response.get("Buckets"):
+                print(bucket.get('Name'))
+    except Exception as e:
+        print(f"Printing buckets operation failed due to this error {e}")
+        return False
+    return True
+
+
+import boto3
+
+import boto3
+import requests
+from botocore.exceptions import NoCredentialsError
+
+def get_default_region():
+    try:
+        session = boto3.Session()
+        default_region = session.region_name
+        if not default_region:
+            # Fetch the region from the metadata service if not set in the config
+            default_region = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone').text[:-1]
+        return default_region
+    except NoCredentialsError as e:
+        print(f"No AWS credentials found: {e}")
+        return None
+
+def create_bucket(bucket_name):
+    try:
+        default_region = get_default_region()
+        if not default_region:
+            print("Unable to determine the default region.")
+            return False
+        
+        s3_client = boto3.client('s3', region_name=default_region)
+        s3_client.create_bucket(Bucket=bucket_name)
+        return True
+    except Exception as e:
+        print(f"Bucket creation failed due to this error: {e}")
+        return False
+
+
+def upload_file(file_name,bucket,object_name=None):
+    if object_name is None:
+        object_name = bucket
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name,bucket,object_name)
+    except Exception as e:
+        return False
+    return True
+
+def delete_file(bucket, key_name):
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.delete_object(Bucket=bucket,Key=key_name)
+    except Exception as e:
+        return False
+    return True
---
+++ src/api/results/results_operations.py
@@ -0,0 +1,100 @@
+from scripts import find_aggregate, get_db_client
+db_client = get_db_client()
+
+def get_results(year: int):
+    """
+    Retrieves student results for a given year from the database and prepares an API response.
+
+    Args:
+    - year (int): The year for which student results are to be retrieved.
+
+    Returns:
+    - dict: A dictionary containing the API response.
+      - 'code' (int): The status code of the response.
+      - 'data' (list or str): If data is available, a list of student summaries. Otherwise, a message indicating no data is available.
+    """
+
+    results_collection = db_client.get_collection("results")
+    db_result = list(results_collection.find({"Year": str(year)}, {"_id": False}))
+    api_response = []
+    for resp in db_result:
+        student_summary = find_aggregate(resp)
+        api_response.append(student_summary)
+    if db_result == []:
+        return {"code": 200, "data": "No Data Available"}
+    return {"code": 200, "data": api_response}
+
+
+
+def get_stats_in_year(year:int):
+    """
+    Retrieves statistics for a given year from the database.
+
+    Args:
+    - year (int): The year for which statistics are to be retrieved.
+
+    Returns:
+    - dict: A dictionary containing statistics:
+        - "studentCount": Total number of students in the given year.
+        - "gt570": Number of students with a total score greater than or equal to 570.
+        - "gt550": Number of students with a total score greater than or equal to 550.
+        - "gt500": Number of students with a total score greater than or equal to 500.
+        - "passPercentage": Overall pass percentage (temporarily hardcoded to 100%).
+    """
+    SCORE_GT_570 = 570
+    SCORE_GT_550 = 550
+    SCORE_GT_500 = 500
+
+    results_collection = db_client.get_collection("results")
+    db_result = list(results_collection.find({"Year": str(year)}, {"_id": False}))
+    total_student_count = len(db_result)
+    count_greater_than_570 = sum(1 for result in db_result if int(result.get('Total')) >= SCORE_GT_570)
+    count_greater_than_550 = sum(1 for result in db_result if int(result.get('Total')) >= SCORE_GT_550)
+    count_greater_than_500 = sum(1 for result in db_result if int(result.get('Total')) >= SCORE_GT_500)
+    
+    pass_percentage = 100  # Temporarily hardcoding to 100%. We need to have failed candidates data in db.
+    
+    api_response = {
+        "studentCount": total_student_count,
+        "gt570": count_greater_than_570,
+        "gt550": count_greater_than_550,
+        "gt500": count_greater_than_500,
+        "passPercentage": pass_percentage
+    }  
+    return {"code": 200, "data": api_response}
+
+
+
+
+def get_toppers_of_year(year:int):
+    """
+    Retrieves top-performing students of a specific year from the database.
+
+    Args:
+    - year (int): The year for which to retrieve top-performing students.
+
+    Returns:
+    - dict: A dictionary containing information about the top 3 performing students:
+        - "code": HTTP status code (200 for successful retrieval).
+        - "data": A list containing information about each top-performing student.
+            Each item in the list is a dictionary with student information:
+                - Example format of student_summary:
+                    {
+                        'student_name': 'John Doe',
+                        'total_score': 590,
+                        'year': 2023,
+                    }
+    """
+    pipeline = [
+        {'$match': {'Year': str(year)}},
+        {'$sort': {'Total': -1}},
+        {'$limit': 3}
+    ]
+
+    results_collection = db_client.get_collection('results')
+    top_students = list(results_collection.aggregate(pipeline))
+
+    
+    top_students_summary = [find_aggregate(student) for student in top_students]
+
+    return {"code": 200, "data": top_students_summary}
