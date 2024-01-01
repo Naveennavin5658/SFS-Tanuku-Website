512e7f7218d534b9acbd4000b49f6801ad70c00d
Naveen <ssrikar1@ford.com>
2024-01-01 23:07:06 +0530

    Revert "Revert "added apis for results and alumni pages""
    This reverts commit 06d076fb7372e090788068ffcb4360c9d179c2e8.

---
+++ enums.py
@@ -0,0 +1,4 @@
+from enum import Enum
+class Env(Enum):
+    MONGO_URL = "MONGO_URL"
+    DATABASE = "DATABASE"
---
+++ src/api/results/results_routes.py
@@ -0,0 +1,31 @@
+from src.api.results import results_blueprint
+from src.api.results.results_operations import get_results, get_stats_in_year, get_toppers_of_year
+
+
+
+@results_blueprint.route('/marks/<year>',methods=["GET"])
+def fetch_results(year):
+    try:
+        response = get_results(year)
+        return response, response['code']
+    except Exception as e:
+        print(f"******FAILED TO FETCH DATA DUE TO ERROR {e}******")
+        
+        
+@results_blueprint.route('/stats/<year>',methods=['GET'])
+def fetch_stats_of_year(year):
+    try:
+        response = get_stats_in_year(year)
+        return response, response['code']
+    except Exception as e:
+        print(f"******FAILED TO FETCH DATA DUE TO ERROR {e}******")
+        
+        
+@results_blueprint.route('/toppers/<year>',methods = ['GET'])
+def fetch_toppers_of_year(year):
+    try:
+        response = get_toppers_of_year(year)
+        return response, response['code']
+    except Exception as e:
+        print(f"******FAILED TO FETCH DATA DUE TO ERROR {e}******")
+        
